cbdfdd443ec494dc764e34542ca510d3b6311406
MariamNsubuga <mariam.nakanyike@gmail.com>
2022-09-15 00:27:29 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main(void)
+{
+	int str[] = { 1,2,3,4,5,6,7,,8,9};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count =0; count <sz; count++)
+	{
+		-putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
+}

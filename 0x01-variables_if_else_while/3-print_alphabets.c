4f445cee182b7df36560d4c751f24e691e2e2ce4
MariamNsubuga <mariam.nakanyike@gmail.com>
2022-09-17 13:51:02 -0700

    tasks 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,19 @@
-#include 
+#include <stdio.h>
+/**
+ * main - prints lower and upper alphabets
+ * Return: always (success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c<= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

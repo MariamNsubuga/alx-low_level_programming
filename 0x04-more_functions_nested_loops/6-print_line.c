6ced15d80eefb01a55647c02b8f139a670883034
MariamNsubuga <mariam.nakanyike@gmail.com>
2022-09-17 15:38:41 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @a: parameter
+ * Return:returns nothing
+ */
+
+void print_line(int a)
+{
+	while (a-- > 0)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
